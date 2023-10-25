b58d78f4aba690160f96d809f4e953f734b2ebb3
Nikola Markovic <nikolageokg@gmail.com>
2023-10-25 23:35:09 +0200

    Struktura foldera i register stranica
    Kreirani folderi "screens" i "viewmodels" i stranica za registraciju

---
+++ src/Front/app/src/main/java/com/example/front/screens/RegisterScreen.kt
@@ -0,0 +1,31 @@
+package com.example.front.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RegisterScreen() {
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ){
+        Text(text = "Registracija")
+    }
+}
+
+@Preview()
+@Composable
+fun prikaziRegister(){
+    RegisterScreen()
+}
