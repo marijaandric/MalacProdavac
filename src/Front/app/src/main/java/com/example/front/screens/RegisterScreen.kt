a677257c3baf96b5b68fd7ab65426277d66a5d21
Nikola Markovic <nikolageokg@gmail.com>
2023-10-26 01:00:26 +0200

    Update RegisterScreen.kt

--- src/Front/app/src/main/java/com/example/front/screens/RegisterScreen.kt
+++ src/Front/app/src/main/java/com/example/front/screens/RegisterScreen.kt
@@ -2,6 +2,11 @@ package com.example.front.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -9,8 +14,12 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterScreen() {
     var name by remember { mutableStateOf("") }
@@ -20,11 +29,47 @@ fun RegisterScreen() {
     Column(
         modifier = Modifier.fillMaxSize()
     ){
-        Text(text = "Registracija")
+        Text(
+            text = "Registracija"
+        )
+
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Ime") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Šifra") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions.Default.copy(
+                imeAction = ImeAction.Done,
+                keyboardType = KeyboardType.Password
+            ),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Button(
+            onClick = {
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Registruj se")
+        }
     }
 }
 
-@Preview()
+@Preview
 @Composable
 fun prikaziRegister(){
     RegisterScreen()
