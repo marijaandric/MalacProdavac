7057ce12fb172706c1cf8aafe6158f064193ae62
milovan.samardzic <kokaroka9@gmail.com>
2023-10-26 03:05:07 +0200

    loginPageIzmene

--- src/Front/app/build.gradle.kts
+++ src/Front/app/build.gradle.kts
@@ -78,4 +78,7 @@ dependencies {
 
     // pager
     implementation("androidx.compose.foundation:foundation:1.4.0")
+
+    //okhttp3
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
 }
---
+++ src/Front/app/src/main/java/com/example/front/YourApiResponse.kt
@@ -0,0 +1,5 @@
+package com.example.front
+
+data class YourApiResponse(
+    val message: String
+)
