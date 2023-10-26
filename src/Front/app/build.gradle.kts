32794eb399ac0b49e136fe45ff21bf6c943b74e2
milovan.samardzic <kokaroka9@gmail.com>
2023-10-26 02:32:29 +0200

    loginPage

--- src/Front/app/build.gradle.kts
+++ src/Front/app/build.gradle.kts
@@ -48,6 +48,7 @@ android {
 
 dependencies {
 
+    implementation("com.github.kittinunf.fuel:fuel:3.0.0-alpha1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -63,4 +64,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-jackson:2.9.0")
+
 }
