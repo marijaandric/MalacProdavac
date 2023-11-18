ac410ea54fddf3f72457bdec647cf9a0aa586459
Nikola Markovic <nikolageokg@gmail.com>
2023-11-18 15:31:41 +0100

    sidebar izmene

--- src/Front/app/src/main/java/com/example/front/MainActivity.kt
+++ src/Front/app/src/main/java/com/example/front/MainActivity.kt
@@ -4,11 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,11 +26,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
@@ -176,23 +182,41 @@ import javax.inject.Inject
                                             shape = RectangleShape
                                         )
                                     }
-
-                                    Text(
-                                        text = "Logout",
+                                    Spacer(modifier = Modifier.weight(1f))
+                                    Row(
                                         modifier = Modifier
-                                            .clickable {
-                                                //pokaze modal da potvrdi da zeli da se izloguje
-                                                //ukloni token
-                                                //vodi na Login
-                                                runBlocking {
-                                                    dataStoreManager.storeToken("")
-                                                }
-                                                val intent = Intent(this@MainActivity, MainActivity::class.java)
-                                                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
-                                                startActivity(intent)
-                                                finish()
-                                            }
+                                            .fillMaxWidth()
+//                                            .height(24.dp)
+                                            .padding(start = 12.dp, bottom = 12.dp)
+                                        ,
+                                        verticalAlignment = Alignment.CenterVertically
                                     )
+                                    {
+                                        Image(
+                                            painter = painterResource(id = R.drawable.logout),
+                                            contentDescription = null,
+                                            modifier = Modifier.height(24.dp)
+                                        )
+                                        Text(
+                                            text = "Logout",
+                                            modifier = Modifier
+                                                .clickable {
+                                                    //pokaze modal da potvrdi da zeli da se izloguje
+                                                    //ukloni token
+                                                    //vodi na Login
+                                                    runBlocking {
+                                                        dataStoreManager.storeToken("")
+                                                    }
+                                                    val intent = Intent(this@MainActivity, MainActivity::class.java)
+                                                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
+                                                    startActivity(intent)
+                                                    finish()
+                                                }
+                                            ,
+                                            fontSize = 16.sp,
+                                            color = Color.White
+                                        )
+                                    }
                                 }
                             },
                             content = {
