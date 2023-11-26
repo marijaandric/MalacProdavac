672ea85ce781f76d66ae6c1fbf3e39e3f0115f07
Nikola Markovic <nikolageokg@gmail.com>
2023-11-27 00:39:06 +0100

    Izmene izgleda

--- src/Front/app/src/main/java/com/example/front/screens/cart/Cart.kt
+++ src/Front/app/src/main/java/com/example/front/screens/cart/Cart.kt
@@ -1,6 +1,7 @@
 package com.example.front.screens.cart
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -16,6 +17,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Surface
 import androidx.compose.material3.ButtonDefaults
@@ -32,6 +34,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -134,8 +137,21 @@ fun Cart(
                             fontWeight = FontWeight.Bold,
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(16.dp)
+                                .padding(top = 16.dp, bottom = 8.dp)
+
                         )
+                    }
+
+                    item {
+                        //Spacer(modifier = Modifier.height(8.dp))
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .height(1.dp)
+                                .background(Color(0xFF3A5163))
+                                .padding(horizontal = 8.dp)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
                     }
 
                     // Prikaz svakog proizvoda iz trenutne prodavnice
@@ -144,17 +160,26 @@ fun Cart(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(4.dp)
+                                .height(96.dp)
+
+                            ,
                         ) {
                             Image(painter = rememberAsyncImagePainter(model = "http://softeng.pmf.kg.ac.rs:10015/images/${product.image}"), contentDescription = "",
                                 modifier = Modifier
                                     .width(96.dp)
                                     .height(96.dp)
-                                    .border(1.dp, Color.Red, RectangleShape)
+//                                    .border(1.dp, Color.Red, RectangleShape)
+                                    .graphicsLayer(
+                                        clip = true,
+                                        shape = RoundedCornerShape(16.dp)
+                                    )
+
                             )
                             Column (
                                 modifier = Modifier
                                     .fillMaxSize()
-                                    .padding(horizontal = 16.dp, vertical = 2.dp)
+                                    .padding(horizontal = 16.dp, vertical = 8.dp)
+//                                    .border(1.dp, Color.Red, RectangleShape)
                                 ,
                                 verticalArrangement = Arrangement.SpaceBetween
                             ) {
@@ -179,6 +204,7 @@ fun Cart(
 
                     item {
                         Spacer(modifier = Modifier.height(16.dp))
+
                     }
                 }
             }
