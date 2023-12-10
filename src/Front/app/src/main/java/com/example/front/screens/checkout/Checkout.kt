ffd56160a27d0074cc9e17b353bc7ea07519528f
Nikola Markovic <nikolageokg@gmail.com>
2023-12-10 21:10:40 +0100

    Napravljen ModalDatePicker

--- src/Front/app/src/main/java/com/example/front/screens/checkout/Checkout.kt
+++ src/Front/app/src/main/java/com/example/front/screens/checkout/Checkout.kt
@@ -10,27 +10,31 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -52,6 +56,35 @@ import com.example.front.navigation.Screen
 import com.example.front.screens.cart.CreditCard
 import com.example.front.viewmodels.checkout.CheckoutViewModel
 import com.google.gson.JsonParser
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ModalDatePicker(
+    onAccept: (Long?) -> Unit,
+    onCancel: () -> Unit
+) {
+    val state = rememberDatePickerState()
+
+    DatePickerDialog(
+        onDismissRequest = onCancel,
+        confirmButton = {
+            Button(onClick = { onAccept(state.selectedDateMillis) }) {
+                Text("Accept")
+            }
+        },
+        dismissButton = {
+            Button(onClick = onCancel) {
+                Text("Cancel")
+            }
+        }
+    ) {
+        DatePicker(state = state)
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -65,6 +98,10 @@ fun CheckoutScreen(
         val shopsTotals = parseTotalsByShop(totalsByShop ?: "")
         viewModel.getCheckoutData(shopsTotals)
     }
+
+    val date = remember { mutableStateOf(LocalDate.now())}
+    val isOpen = remember { mutableStateOf(false)}
+
 
     val checkoutState = viewModel.state.value
     val shops = viewModel.shopsForCheckout.value
@@ -87,6 +124,40 @@ fun CheckoutScreen(
                         .fillMaxSize()
                 ) {
                     SmallElipseAndTitle(title = "Checkout", drawerState)
+
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+
+                        OutlinedTextField(
+                            readOnly = true,
+                            value = date.value.format(DateTimeFormatter.ISO_DATE),
+                            label = { Text("Date") },
+                            onValueChange = {})
+
+                        IconButton(
+                            onClick = { isOpen.value = true } // show de dialog
+                        ) {
+                            Icon(imageVector = Icons.Default.DateRange, contentDescription = "Calendar")
+                        }
+                    }
+                    if (isOpen.value) {
+                        ModalDatePicker(
+                            onAccept = {
+                                isOpen.value = false // close dialog
+
+                                if (it != null) { // Set the date
+                                    date.value = Instant
+                                        .ofEpochMilli(it)
+                                        .atZone(ZoneId.of("UTC"))
+                                        .toLocalDate()
+                                }
+                            },
+                            onCancel = {
+                                isOpen.value = false //close dialog
+                            }
+                        )
+                    }
+//                    DatePicker(state = datePickerState, )
+
                         //kartice
                         LazyRow(
                             modifier = Modifier
@@ -251,8 +322,6 @@ fun CheckoutScreen(
                                 // opciono datepicker
                                 if (shop.selfpickup) {
                                     Text(text = "Biranje datuma")
-//                    TextField(value = "", onValueChange = {})
-//                                    }
                                 }
                             }
                         }
