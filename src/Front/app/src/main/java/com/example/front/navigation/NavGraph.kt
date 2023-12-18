78245e4e9c07999a40ea92bb203b670c31533355
Marija Andric <marijaandric2001@gmail.com>
2023-12-18 02:12:27 +0100

    Male promene

--- src/Front/app/src/main/java/com/example/front/navigation/NavGraph.kt
+++ src/Front/app/src/main/java/com/example/front/navigation/NavGraph.kt
@@ -70,8 +70,8 @@ fun SetupNavGraph(
 
     NavHost(
         navController = navController,
-        //startDestination = "intro"
-        startDestination = "products"
+        startDestination = "intro"
+        //startDestination = "my_shop"
     ) {
         composable(
             route = Screen.Home.route
